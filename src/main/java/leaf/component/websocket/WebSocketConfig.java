e4d4f2ba40d8ae85c67dee338c043e0d067852cd
sionel <sadb0101@naver.com>
2019-11-18 17:31:37 +0900

    websocket

--- src/main/java/leaf/component/websocket/WebSocketConfig.java
+++ src/main/java/leaf/component/websocket/WebSocketConfig.java
@@ -21,8 +21,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic/public");
+        registry.setApplicationDestinationPrefixes("/chat");
+        registry.enableSimpleBroker("/chat/message");
     }
 
 }
